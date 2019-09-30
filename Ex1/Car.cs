20cf9d9c9836bd98a1290e1295e8bf8f9ec03db6
S00188447 <47103333+S00188447@users.noreply.github.com>
2019-09-30 12:41:44 +0100

    Add project files.

---
+++ Ex1/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    public class Car
+    {
+        private string make;
+
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+
+        private string _model;
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        private int currentSpeed;
+
+        public int CurrentSpeed
+        {
+            get { return currentSpeed; }
+            set { currentSpeed = value; }
+        }
+
+        private double engineSize;
+
+        public double EngineSize
+        {
+            get { return engineSize; }
+            set { engineSize = value; }
+        }
+
+        public void DisplayCarInfo()
+        {
+            Console.WriteLine($"Car Make: {Make}");
+            Console.WriteLine($"Car Model: {Model}");
+            Console.WriteLine($"Car Current Speed: {CurrentSpeed}");
+            Console.WriteLine($"Car Engine Size: {EngineSize}");
+
+        }
+
+        public override string ToString()
+        {
+            return
+            string.Format($"Car Make: {Make}" +
+            $"\nCar Model: {Model}") +
+            $"\nCar Current Speed: {CurrentSpeed}" +
+            $"\nCar Engine Size: {EngineSize}";
+        }
+
+        public void Accelerate()
+        {
+            CurrentSpeed += 10;
+            Console.WriteLine($"Current Speed is {CurrentSpeed}");
+
+        }
+    }
+}
---
+++ Ex1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car c1 = new Car();
+            c1.Make = "Ford";
+            c1.Model = "Focus";
+            c1.CurrentSpeed = 0;
+            c1.EngineSize = 1.6;
+
+            Car c2 = new Car();
+            c2.Make = "Opel";
+            c1.Model = "Astra";
+            c1.CurrentSpeed = 0;
+            c1.EngineSize = 1.2;
+
+            c1.DisplayCarInfo();
+
+            Console.WriteLine(c2);
+
+            for(int i = 0; i <10; i++)
+            {
+                c1.Accelerate();
+            }
+
+            Console.WriteLine(c1);
+
+
+        }
+    }
+}
+
